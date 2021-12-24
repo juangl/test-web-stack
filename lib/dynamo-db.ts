f4f04340454d1526c8e19c57d40f8fb767f26af2
Juan Je Garcia <juanjegarcialopez@gmail.com>
2021-12-23 22:48:03 -0600

    fix dynamoDB helper types

--- lib/dynamo-db.ts
+++ lib/dynamo-db.ts
@@ -4,28 +4,31 @@ const client = new aws.DynamoDB.DocumentClient({
   accessKeyId: process.env.ACCESS_KEY,
   secretAccessKey: process.env.SECRET_KEY,
   region: process.env.REGION,
-  params: {
-    TableName: process.env.TABLE_NAME,
-  },
 });
 
-// utility type to exclude `TableName` from the payload since the TableName is assigned by default at the end moment the client is created
+// utility type extract the API call param types and make `TableName` optional
 type DynamoDBAPICallParams<T extends (...args: any) => any> = Omit<
   Parameters<T>[0],
   "TableName"
->;
+> & { TableName?: string };
+
+// helper to assign TableName with a default value
+const prepareDynamoDBParams = <T>(params: T) => ({
+  TableName: process.env.TABLE_NAME,
+  ...params,
+});
 
 const API = {
   get: (params: DynamoDBAPICallParams<typeof client.get>) =>
-    client.get(params).promise(),
+    client.get(prepareDynamoDBParams(params)).promise(),
   put: (params: DynamoDBAPICallParams<typeof client.put>) =>
-    client.put(params).promise(),
+    client.put(prepareDynamoDBParams(params)).promise(),
   query: (params: DynamoDBAPICallParams<typeof client.query>) =>
-    client.query(params).promise(),
+    client.query(prepareDynamoDBParams(params)).promise(),
   update: (params: DynamoDBAPICallParams<typeof client.update>) =>
-    client.update(params).promise(),
+    client.update(prepareDynamoDBParams(params)).promise(),
   delete: (params: DynamoDBAPICallParams<typeof client.delete>) =>
-    client.delete(params).promise(),
+    client.delete(prepareDynamoDBParams(params)).promise(),
 };
 
 export default API;
