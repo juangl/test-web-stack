c08ec651ff2628bf7417ef075048b0e01a43aca2
Juan Je Garcia <juanjegarcialopez@gmail.com>
2021-12-27 12:41:04 -0600

    format files

--- lib/graphqlSchema.ts
+++ lib/graphqlSchema.ts
@@ -1,9 +1,8 @@
-
-import { makeExecutableSchema } from '@graphql-tools/schema'
-import * as resolvers from './graphqlResolvers'
-import { typeDefs  } from './graphqlTypeDefs'
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import * as resolvers from "./graphqlResolvers";
+import { typeDefs } from "./graphqlTypeDefs";
 
 export const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
-})
+});
