46e3666d99ea1f6c81093bb965366079f346dacd
Juan Je Garcia <juanjegarcialopez@gmail.com>
2021-12-28 16:57:35 -0600

    didn't work. Nevermind

--- components/userList.tsx
+++ components/userList.tsx
@@ -48,7 +48,7 @@ export function UserList(props: UserListProps) {
   // fetch data
   const { loading, data, fetchMore } = useQuery(ALL_USERS_QUERY, {
     variables: getInitialPaginationVariables(initialPage, props.search),
-    fetchPolicy: "cache-and-network",
+    fetchPolicy: "network-only",
     nextFetchPolicy: "cache-first",
   });
 
